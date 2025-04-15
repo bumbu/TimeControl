c58032f14e8f0aed599b8ba194004bcaf7933ca7
bumbu <bmbalex@gmail.com>
2025-04-15 09:32:06 +0100

    Check for screen status. Lock the screen when time comes

--- index.js
+++ index.js
@@ -3,18 +3,30 @@ var spawn = require('child_process').spawn;
 var os = require('os');
 var path = require('path');
 var fs = require("fs");
+var exec = require('child_process').exec;
+var execSync = require('child_process').execSync;
 
 var IS_DEBUG = false;
 
-console.log("[" + getCurrentDateTime() + "] Starting time control");
-
 var timeControlPath = path.join(os.homedir(), 'Documents', 'time_control.txt');
 
-if (shouldAskForTime()) {
-  askForTime();
-} else if (shouldNotifyOfTime()) {
-  notifyOfTime();
-}
+// Test Screen lock logic
+// setInterval(function() {
+//   console.log('isScreenLocked', isScreenLocked());
+// }, 1000)
+
+var isLocked = isScreenLocked();
+console.log("[" + getCurrentDateTime() + "] Starting time control. Screen lock state: " + isLocked);
+
+// Only ask for time or notify of time when the screen is unlocked
+if (isLocked === false) {
+  if (shouldAskForTime()) {
+    askForTime();
+  } else if (shouldNotifyOfTime()) {
+    notifyOfTime();
+  }
+}  
+
 
 function shouldAskForTime() {
   var now = new Date();
@@ -115,6 +127,24 @@ function notifyOfTime() {
     var script = `tell application "System Events" to display dialog "Your time is out, as you set it to ${timeValue}" with title "Time Control" buttons "OK" with icon 2 giving up after 45`;
     run("osascript", ["-e", script], function (code) {
       console.log('Notified user', code);
+
+      var scriptLock = `
+activate application "SystemUIServer"
+tell application "System Events"
+    tell process "SystemUIServer" to keystroke "q" using {command down, control down}
+end tell
+      `;
+      var isLocked = isScreenLocked();
+      if (isLocked === true) {
+        console.log('Trying to lock the screen', code);  
+        run("osascript", ["-e", scriptLock], function (code) {
+          console.log('Locked the screen', code);
+        });
+      } else if (isLocked === false) {
+        console.log('Screen is already locked');
+      } else {
+        console.log('Unknown screen lock state. Not locking');
+      }
     });
 
     function run(bin, args, cb) {
@@ -127,6 +157,45 @@ function notifyOfTime() {
       });
     }
 }
+
+// function isScreenLockedCb(cb) {
+//   // https://www.reddit.com/r/applescript/comments/det3go/check_if_mac_is_awake_before_unlocking_with_ssh/
+//   // Script that returns 0 when screen is locked 
+//   var screenLockScript = `[ "$(/usr/libexec/PlistBuddy -c "print :IOConsoleUsers:0:CGSSessionScreenIsLocked" /dev/stdin 2>/dev/null <<< "$(ioreg -n Root -d1 -a)")" = "true" ] && echo 0 || echo 1;`;
+//   exec(screenLockScript, function(err, stdout, stderr) {
+//     if (err) {
+//       return null;
+//     }
+//     // console.log(stdout);
+//     if (stdout === '0') {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   });
+// }
+
+function isScreenLocked(cb) {
+  // https://www.reddit.com/r/applescript/comments/det3go/check_if_mac_is_awake_before_unlocking_with_ssh/
+  // Script that returns 0 when screen is locked 
+  var screenLockScript = `[ "$(/usr/libexec/PlistBuddy -c "print :IOConsoleUsers:0:CGSSessionScreenIsLocked" /dev/stdin 2>/dev/null <<< "$(ioreg -n Root -d1 -a)")" = "true" ] && echo 0 || echo 1;`;
+  var isLocked = null;
+  try {
+    var output = execSync(screenLockScript, {timeout: 5 * 1000}).toString().trim();
+    if (output === '0') {
+      isLocked = true;
+    } else if (output === '1') {
+      isLocked = false;
+    } else {
+      console.log(`Received ${output} when checking for screen status`);
+    }
+  } catch {
+    console.log(`Checking for screen status has thrown`);
+  }
+  return isLocked;
+}
+
+
 
 function getCurrentDateTime() {
   var currentdate = new Date();
